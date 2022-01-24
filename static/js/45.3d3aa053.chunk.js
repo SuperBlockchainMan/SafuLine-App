a0af7419e015db1bf5ce93ef64572193b7902c7d
SuperBlockchainMan <talentdeveloper59@gmail.com>
2022-01-24 16:49:28 +0900

    first version

---
+++ static/js/45.3d3aa053.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[45],{1657:function(e,c,t){"use strict";t.r(c);t(1);var n=t(2),s=t(13),r=t(229),a=t(440),j=t(1207),b=t(0);c.default=function(){var e=Object(s.b)().t;return Object(b.jsxs)(b.Fragment,{children:[Object(b.jsx)(a.a,{children:Object(b.jsx)(n.gb,{as:"h1",scale:"xxl",color:"secondary",children:e("Activity")})}),Object(b.jsx)(r.b,{children:Object(b.jsx)(n.A,{children:Object(b.jsx)(j.a,{})})})]})}}}]);
+//# sourceMappingURL=45.3d3aa053.chunk.js.map
